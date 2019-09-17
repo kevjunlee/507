b2f376f5b20a62bcd2f7615b69db4d4bc950a151
Ashwin Malik <ashwinmalik92@gmail.com>
2019-09-17 10:35:28 -0400

    added stuff and print statements

--- hello.py
+++ hello.py
@@ -1,5 +1,16 @@
-"My name is Kevin"
-"My name is Ashwin"
+# Names
+print("My name is Kevin")
+print("My name is Ashwin")
 
-"I was born in Malaysia"
-"I was born in Chicago"
+# Birthplaces
+print("I was born in Chicago")
+print("I was born in Malaysia")
+
+# Food
+print("Roast duck")
+
+# Music
+print("Rock")
+
+# Astrology
+print("Gemini")
